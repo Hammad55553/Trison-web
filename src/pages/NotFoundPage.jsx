10c74133b2e73edef0481d134d54d67cae09a83c
Hammad55553 <hammadaslam78612@gmail.com>
2026-08-05 02:48:11 +0500

    feat: add beautiful animated solar graphic to 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -6,8 +6,15 @@ const NotFoundPage = () => {
   return (
     <div className="notfound-container">
       <div className="notfound-content">
-        <div className="notfound-icon-wrapper">
-          <ZapOff size={80} className="notfound-icon" />
+        <div className="notfound-animation-container">
+          <div className="solar-core"></div>
+          <div className="orbit orbit-1"></div>
+          <div className="orbit orbit-2"></div>
+          <div className="orbit orbit-3"></div>
+          <div className="particle particle-1"></div>
+          <div className="particle particle-2"></div>
+          <div className="particle particle-3"></div>
+          <ZapOff size={40} className="notfound-icon-center" />
         </div>
         <h1 className="notfound-title">404</h1>
         <h2 className="notfound-subtitle">Energy Depleted. Page Not Found.</h2>
